f538387202d52419aa98c606c88db48f6223730f
Dipesh Raj <draj346@gmail.com>
2024-06-18 20:46:45 +0530

    Created Custom Sitecore Lazy Load Field

---
+++ src/Foundation/RichText/website/Fields/LazyLoadImage.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Web.UI.Sheer;
+
+namespace Learning.Foundation.RichText.Fields
+{
+    public class LazyLoadImage : Image
+    {
+        public override void HandleMessage(Message message)
+        {
+            base.HandleMessage(message);
+        }
+
+    }
+}
